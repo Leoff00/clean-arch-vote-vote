4c78e1e280e871b7b3d06cfb8a40eee50612c207
leoff00 <leof0180@gmail.com>
2023-01-16 12:10:12 -0300

    chore: configure vite to resolves paths aliases

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,18 +1,14 @@
 /// <reference types="vitest" />
 
 import { defineConfig } from "vitest/config";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
+  plugins: [tsconfigPaths()],
   test: {
-    include: [
-      "src/**/*.{spec,test}.ts"
-    ],
+    include: ["src/**/*.{spec,test}.ts"],
     coverage: {
-      reporter: [
-        "text", 
-        "html", 
-        "lcov"
-      ],
+      reporter: ["text", "html", "lcov"],
     },
   },
 });
