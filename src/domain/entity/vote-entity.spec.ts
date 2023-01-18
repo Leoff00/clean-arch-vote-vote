8298a71fdcb1ecea577fb304bdc79645bf9234d4
leoff00 <leof0180@gmail.com>
2023-01-17 21:32:11 -0300

    feat: adding new entity tests

---
+++ src/domain/entity/vote-entity.spec.ts
@@ -0,0 +1,39 @@
+import { describe, expect, it } from "vitest";
+import { VoteEntity } from "./vote-entity";
+import { randomUUID } from "node:crypto";
+
+describe("Vote Entity Business Rules", () => {
+  it("Should check if user votes", () => {
+    const mock = {
+      id: randomUUID(),
+      singleVote: 1,
+    };
+    const vote = new VoteEntity(mock.id, mock.singleVote);
+
+    expect(vote.hasVoted()).toBe(true);
+  });
+
+  it("Should check if vote has reached max limit", () => {
+    const mock = {
+      id: randomUUID(),
+      singleVote: 10,
+    };
+
+    const vote = new VoteEntity(mock.id, mock.singleVote);
+
+    expect(vote.hasReachedMaxVotation()).toBe(true);
+  });
+
+  it("Should check if vote has reached max limit", () => {
+    const mock = {
+      id: randomUUID(),
+      singleVote: 11,
+    };
+
+    const vote = new VoteEntity(mock.id, mock.singleVote);
+
+    expect(vote.hasExceededMaxTimesVote()).toThrowError(
+      "Você ultrapassou o limite de votação!"
+    );
+  });
+});
--- src/domain/entity/vote-entity.ts
+++ src/domain/entity/vote-entity.ts
@@ -1,16 +1,38 @@
 export class VoteEntity {
   private readonly MAX_TIMES_VOTE: number = 10;
+  id: string;
+  singleVote = 0;
 
-  constructor(
-    private readonly id: string,
-    private readonly singleVote: number = 0
-  ) {}
+  constructor(id: string, singleVote: number) {
+    this.id = id;
+    this.singleVote = singleVote;
+  }
+
+  get MaxTimesVote() {
+    return this.MAX_TIMES_VOTE;
+  }
 
   hasVoted(): boolean {
     return this.singleVote > 0 && this.singleVote <= this.MAX_TIMES_VOTE;
   }
 
+  hasExceededMaxTimesVote(): void {
+    if (this.singleVote > this.MAX_TIMES_VOTE) {
+      throw (
+        (new Error("Você ultrapassou o limite de votação!"),
+        (this.singleVote = 10))
+      );
+    }
+  }
+
   hasReachedMaxVotation(): boolean {
     return this.singleVote === this.MAX_TIMES_VOTE;
+  }
+
+  toJSON() {
+    return {
+      id: this.id,
+      singleVote: this.singleVote,
+    };
   }
 }
