baa6940ee04c25ab93054bc2f2845f06cd0e3ff9
leoff00 <leof0180@gmail.com>
2023-01-18 22:51:32 -0300

    chore: configure and fix vitest issues

--- vitest.config.ts
+++ vitest.config.ts
@@ -6,7 +6,6 @@ import tsconfigPaths from "vite-tsconfig-paths";
 export default defineConfig({
   plugins: [tsconfigPaths()],
   test: {
-    include: ["src/**/*.{spec,test}.ts"],
     coverage: {
       reporter: ["text", "html", "lcov"],
     },
