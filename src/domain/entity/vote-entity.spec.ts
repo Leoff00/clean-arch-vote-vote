0fd4c4505ac351a2f16b8760d60504c86e0c6872
leoff00 <leof0180@gmail.com>
2023-01-25 20:34:41 -0300

    style: indent and adjust tab spaces

--- src/domain/entity/vote-entity.spec.ts
+++ src/domain/entity/vote-entity.spec.ts
@@ -1,6 +1,5 @@
 import { describe, expect, it } from "vitest";
 import { VoteEntity } from "./vote-entity";
-import { randomUUID } from "node:crypto";
 import { voteMocksFactory } from "@/mocks/vote-mocks";
 
 describe("Vote Entity Business Rules", () => {
@@ -21,7 +20,6 @@ describe("Vote Entity Business Rules", () => {
   it("Should check if vote throws an exception and singleVote receives 10", () => {
     const { id, singleVote } = voteMocksFactory({ singleVote: 11 });
     const vote = new VoteEntity(id, singleVote);
-
     expect(() => vote.hasExceededMaxTimesVote()).toThrowError(
       "Você ultrapassou o limite de votação!"
     );
