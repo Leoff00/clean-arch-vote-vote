56542f0eb204f53b5dc8e7d94c4ab853a3451731
leoff00 <leof0180@gmail.com>
2023-01-26 12:02:24 -0300

    refactor: submit vote use case receives an object from params

--- src/application/usecases/vote-usecase/submit-vote.ts
+++ src/application/usecases/vote-usecase/submit-vote.ts
@@ -3,10 +3,10 @@ import { VoteDTO } from "@/application/DTOs";
 import { VoteEntity } from "@/domain/entity";
 
 export class SubmitVoteUseCase {
-  constructor(private readonly votationRepository: VoteRepository) { }
+  constructor(private readonly votationRepository: VoteRepository) {}
 
   async execute(input: VoteDTO) {
     const vote = new VoteEntity(input.id, input.singleVote);
-    await this.votationRepository.submitVote(vote.id, vote.singleVote);
+    await this.votationRepository.submitVote(vote);
   }
 }
