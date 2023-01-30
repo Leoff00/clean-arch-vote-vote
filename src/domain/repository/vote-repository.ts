245eceabca7f08db461bd257b8553c6c9fe6e1a4
leoff00 <leof0180@gmail.com>
2023-01-30 00:31:25 -0300

    feat: creating incrementVote method

--- src/domain/repository/vote-repository.ts
+++ src/domain/repository/vote-repository.ts
@@ -3,4 +3,5 @@ import { VoteEntity } from "@/domain/entity";
 export interface VoteRepository {
   submitVote: (vote: VoteEntity) => Promise<void>;
   getVotes: () => Promise<Array<VoteEntity>>;
+  incrementVote: (id: string, singleVote: number) => Promise<void>;
 }
